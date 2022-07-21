4b9cd21a1517a17e31a550056cd8ebe0e4b8225b
ahmedkhaledddd <ahmedkhaledhanfi@gmail.com>
2022-07-21 10:20:23 +0200

    profile feed api with authentication

--- profilesrestapi/profilesapi/urls.py
+++ profilesrestapi/profilesapi/urls.py
@@ -6,8 +6,10 @@ from . import views
 router = DefaultRouter()
 router.register('hello-veiwset', views.HelloViewSet , basename= 'hello-viewset')
 router.register('profile-viewset', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
 path('hello/',views.HelloAPIView.as_view(), name='hello-view'),
+path('login/', views.UserLoginApiView.as_view()),
 path('',include(router.urls)),
 ]
